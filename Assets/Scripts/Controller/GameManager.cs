33fe8937c7662e053a44aaea6ed45ef3017852df
raydjackson <rayray0706@gmail.com>
2022-12-23 11:48:23 -0800

    Sets up the major field zones and tests using GameManager the positions of the playcards in each position

--- Assets/Scripts/Controller/GameManager.cs
+++ Assets/Scripts/Controller/GameManager.cs
@@ -6,13 +6,37 @@ using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    public GameObject basicPlaycard;
+    [SerializeField]
+    public GameObject activePlaycard;
+    public Legend example;
     public Playcard selectedPlaycard = null;
     public LayerMask fieldLayer;
+    //Player 1 Zones
+    public Transform activePlayerOne;
+    public Transform supportLeftPlayerOne;
+    public Transform supportRightPlayerOne;
+    public Transform withdrawOnePlayerOne;
+    public Transform withdrawTwoPlayerOne;
+    public Transform withdrawThreePlayerOne;
+    //Player 2 Zones
+    public Transform activePlayerTwo;
+    public Transform supportLeftPlayerTwo;
+    public Transform supportRightPlayerTwo;
+    public Transform withdrawOnePlayerTwo;
+    public Transform withdrawTwoPlayerTwo;
+    public Transform withdrawThreePlayerTwo;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Playcard pC = Instantiate(basicPlaycard, withdrawOnePlayerTwo.position, Quaternion.identity).GetComponent<Playcard>();
+        Playcard pC2 = Instantiate(basicPlaycard, withdrawTwoPlayerTwo.position, Quaternion.identity).GetComponent<Playcard>();
+        Playcard pC3 = Instantiate(basicPlaycard, withdrawThreePlayerTwo.position, Quaternion.identity).GetComponent<Playcard>();
+        pC.legend = example;
+        pC2.legend = example;
+        pC3.legend = example;
     }
 
     // Update is called once per frame

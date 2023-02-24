c7917929e76eba9e825a7905c710d281a7ce2171
raydjackson <rayray0706@gmail.com>
2023-02-24 10:44:05 -0800

    Base Classes for Techniques

---
+++ Assets/Scripts/Model/LegendFramework/TechPart.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TechPart : MonoBehaviour
+{
+    //name
+    //activate method
+        //activate needs a parameter for number of mods and element option
+}
---
+++ Assets/Scripts/Model/LegendFramework/Technique.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Technique : MonoBehaviour
+{
+    //name
+    //description
+    //TechPart[]
+    //activateOne method, parameter
+    //activateTwo method, parameter
+
+    //string[] techOptions
+}
